09a51e8014e9665c58a131af3cb04c27f11cbcf3
PDM User <pdmuser@Progressives-MacBook-Pro.local>
2017-05-25 10:42:30 -0400

    fixing form

--- themes/shaggy-reynolds/framework/theme-extras.php
+++ themes/shaggy-reynolds/framework/theme-extras.php
@@ -4,5 +4,6 @@ add_filter( 'wpcf7_form_elements', 'progressive_wpcf7_form_elements' );
 function progressive_wpcf7_form_elements( $content ) {
   $content = strip_tags( $content, '<input><textarea><button><label><div><i><small>');
   $content = str_replace('aria-required="true"', 'aria-required="true" required', $content );
+  $content = str_replace('wpcf7-form-control wpcf7-submit', 'wpcf7-form-control wpcf7-submit btn btn--default btn--outline-accent contact__btn', $content );
   return $content;  
 }
