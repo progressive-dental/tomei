4e74ac9ab4d75eef8977f67d3b21eddb94082c2d
PDM User <pdmuser@Progressives-MacBook-Pro.local>
2017-05-25 10:35:55 -0400

    fixing contact form

--- themes/shaggy-reynolds/framework/theme-extras.php
+++ themes/shaggy-reynolds/framework/theme-extras.php
@@ -3,5 +3,6 @@ add_filter( 'wpcf7_form_elements', 'progressive_wpcf7_form_elements' );
 
 function progressive_wpcf7_form_elements( $content ) {
   $content = strip_tags( $content, '<input><textarea><button><label><div><i><small>');
+  $content = str_replace('aria-required="true"', 'aria-required="true" required', $content );
   return $content;  
 }
