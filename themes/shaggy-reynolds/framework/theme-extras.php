7bcafac42a6e30ce8f0475779552a04461aa5a9e
PDM User <pdmuser@Progressives-MacBook-Pro.local>
2017-05-25 09:28:43 -0400

    widget updates

--- themes/shaggy-reynolds/framework/theme-extras.php
+++ themes/shaggy-reynolds/framework/theme-extras.php
@@ -2,6 +2,6 @@
 add_filter( 'wpcf7_form_elements', 'progressive_wpcf7_form_elements' );
 
 function progressive_wpcf7_form_elements( $content ) {
-  $content = strip_tags( $content, '<input><textarea><button><label><div><i>');
+  $content = strip_tags( $content, '<input><textarea><button><label><div><i><small>');
   return $content;  
 }
