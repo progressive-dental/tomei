5d34f2f00134d57d594ddc6cccdbe6235204c0d5
PDM User <pdmuser@Progressives-MacBook-Pro.local>
2017-05-25 10:44:07 -0400

    making comments smaller

--- themes/shaggy-reynolds/framework/theme-extras.php
+++ themes/shaggy-reynolds/framework/theme-extras.php
@@ -5,5 +5,6 @@ function progressive_wpcf7_form_elements( $content ) {
   $content = strip_tags( $content, '<input><textarea><button><label><div><i><small>');
   $content = str_replace('aria-required="true"', 'aria-required="true" required', $content );
   $content = str_replace('wpcf7-form-control wpcf7-submit', 'wpcf7-form-control wpcf7-submit btn btn--default btn--outline-accent contact__btn', $content );
+  $content = str_replace('rows="10"', 'rows="5"', $content );
   return $content;  
 }
